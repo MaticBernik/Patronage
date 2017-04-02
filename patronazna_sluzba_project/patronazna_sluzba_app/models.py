a999c3d6a4b151a03828f42d79ce9a5e03b1519a
maticbernik <mb2551@student.uni-lj.si>
2017-04-02 18:48:17 +0200

    models.py in progress...

--- patronazna_sluzba_project/patronazna_sluzba_app/models.py
+++ patronazna_sluzba_project/patronazna_sluzba_app/models.py
@@ -3,74 +3,105 @@ from datetime import datetime,timedelta
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.contrib.auth.models import Group
+from django.conf import settings
 
-class Author(models.Model):
-	#author_id --> ze po defaultu
-	firstName=models.CharField(max_length=100, null=False)
-	lastName=models.CharField(max_length=100, null=False)
+'''class Osebe(models.Model):
+	emso = models.CharField(max_length=13, null=False, primary_key=True)#raje integer?
+	ime = models.CharField(max_length=100, null=False)
+	priimek = models.CharField(max_length=100, null=False)
+	naslov = models.CharField(max_length=100, null=False)
+	telefon = models.CharField(max_length=15, null=False) #+368
 
+class Registrirani_uporabniki(models.Model):
+	email = models.CharField(max_length=13, null=False, unique=True) #primary_key=True 
+	geslo = 
+	emso = models.ForeignKey(Osebe, on_delete=models.CASCADE)
+
+class Pacienti(models.Model):
+	sifra_okolisa = models.IntegerField()
+	datum_rojstva = models.DateTimeField()
+	st_zdravstvene_kartice = 
+	email = models.ForeignKey(Registrirani_uporabnik,on_delete=models.CASCADE) 
+
+class Zdravstveno_osebje(models.Model):
+	sifra
+	sifra_izvajalca
+	email = models.ForeignKey(Registrirani_uporabnik,on_delete=models.CASCADE)	
+
+class Sorodstvena_razmerja(models.Model):
+	email = models.ForeignKey(Registrirani_uporabnik,on_delete=models.CASCADE)	
+	emso
+	vrsta_razmerja
+
+class Funkcije(models.Model):
+	email = models.ForeignKey(Registrirani_uporabnik,on_delete=models.CASCADE)
+	funkcija
+'''
+
+class Zdravnik(models.Model):
+	uporabniski_profil = models.ForeignKey(User,on_delete=models.CASCADE)
+	sifra_zdravnika = models.IntegerField() #koliko mestna?
+	telefonska_st = models.CharField(max_length=15, null=False) #+368
+	#sifra_izvajalca_ZS
 	def __str__(self):
-		return self.firstName+' '+self.lastName
+		return self.sifra_zdravnika+' '+self.uporabniski_profil.firstName+''+self.uporabniski_profil.lastName
 
-class Book(models.Model):
-	#book_id --> ze po defaultu
-	title = models.CharField(max_length=100, null=False)
-	added_date = models.DateTimeField(default=datetime.now())
-	author = models.ForeignKey(Author,on_delete=models.SET_NULL,null=True)
-	genre = models.CharField(max_length=10,null=True)
-	available = models.BooleanField(default=True)
+class Vodja_PS(models.Model):
+	uporabniski_profil = models.ForeignKey(User, on_delete=models.CASCADE)
+	sifra_vodje_PS = models.IntegerField()
+	telefonska_st = models.CharField(max_length=15, null=False)
+	#sifra_izvajalca_ZS
 
-	def __str__(self):
-		return self.author.firstName+' '+self.author.lastName+': '+self.title
+class Patronazna_sestra(models.Model):
+	uporabniski_profil = models.ForeignKey(User, on_delete=models.CASCADE)
+	sifra_patronazne_sestre = models.IntegerField()
+	telefonska_st = models.CharField(max_length=15, null=False)
+	#sifra_izvajalca_ZS
 
-class Lease(models.Model):
-	#lease_id  --> ze po defaultu
-	user_id= models.ForeignKey(User,on_delete=models.CASCADE) 
-	book_id= models.ForeignKey(Book,on_delete=models.CASCADE)
-	start_date= models.DateTimeField(default=datetime.now())
-	expiration_date = models.DateTimeField(default=datetime.now() + timedelta(days=14))
+class Sodelavec_ZD(models.Model):
+	uporabniski_profil = models.ForeignKey(User, on_delete=models.CASCADE)
+	sifra_sodelavca = models.IntegerField()
+	telefonska_st = models.CharField(max_length=15, null=False)
+	#sifra_izvajalca_ZS
 
-	def is_expired(self):
-		if self.expiration_date < timezone.now():
-			return True
-		else:
-			return False
+'''class Izvajalec_ZS(models.Model):
+	st_izvajalca = models.IntegerField(primary_key=True)
+	sifra_nadrejenega
+	sifra_osnovni
+	sifra_podrejeni
+	sifra_lokacije
+	naziv_organizacije
+	dodatni_naziv
+	naslov
+	posta
+	sifra_VZD
+	naziv_VZD
+	sifra_pravnega_statusa_izvajalca
+	opis_pravnega_statusa_izvajalca
+	sifra_tipa_izvajalca
+	opis_tipa_izvajalca
+	sifra_obcine
+	ime_obcine
+	sifra_ZZV
+	izvajalec_samo1X
+	stevilo_vrstic_VZDjev'''
 
-	def __str__(self):
-		return str(self.start_date)+': '+self.user_id.first_name+' '+self.user_id.last_name+' - '+self.book_id.title
+#class Okolis(models.Model):
 
-class Reservation(models.Model):
-	# reservation_id  --> ze po defaultu
-	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-	book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
-	start_date = models.DateTimeField(default=datetime.now())
-	expiration_date = models.DateTimeField(default=datetime.now() + timedelta(days=14))
 
-	def is_expired(self):
-		if self.expiration_date < timezone.now():
-			return True
-		else:
-			return False
+class Pacient(models.Model):
+	uporabniski_profil = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)#pacient je lahko registriran (lahko pa tudi ne v primeru skrbnistva)
+	telefonska_st = models.CharField(max_length=15, null=False)
+	#sifra_okolisa
+	naslov = models.CharField(max_length=100, null=False)
+	datum_rojstva = models.DateTimeField(default=datetime.now() + timedelta(days=14))
+	spol
 
-	def __str__(self):
-		return 'EXP:'+str(self.expiration_date) + ': ' + self.user_id.first_name + ' ' + self.user_id.last_name + ' - ' + self.book_id.title
 
-class Message(models.Model):
-	text = models.CharField(max_length=200, null=False)
+class Kontaktna_oseba(models.Model):
+	ime = models.CharField(max_length=100, null=False)
+	priimek = models.CharField(max_length=100, null=False)
+	naslov = models.CharField(max_length=100, null=False)
+	telefon = models.CharField(max_length=15, null=False)  # +368
 
-	def __str__(self):
-		if len(self.text) > 10:
-			return self.text[:10]
-		else:
-			return self.text
-
-class Users_Messages(models.Model):
-	user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-	message_id = models.ForeignKey(Message, on_delete=models.CASCADE)
-
-	def __str__(self):
-		s=self.user_id.username+": '"
-		if len(self.message_id.text) >10:
-			return s+self.message_id.text[:10]+"'"
-		else:
-			return s+self.message_id.text+"'"
+class Sorodstveno_razmerje(models.Model):

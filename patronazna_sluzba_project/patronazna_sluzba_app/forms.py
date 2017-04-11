987e693e7f5e315999a9182851890006dcc1f850
deyvidatanasovski <gedoumazo@gmail.com>
2017-04-11 18:05:39 +0200

    Posodobljena forma

--- patronazna_sluzba_project/patronazna_sluzba_app/forms.py
+++ patronazna_sluzba_project/patronazna_sluzba_app/forms.py
@@ -7,7 +7,17 @@ class LoginForm(forms.Form):
   username = forms.CharField(label='Username:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
   password = forms.CharField(max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
   
-
+SEX_CHOICES = (
+    ('1', 'Moški'),
+    ('2', 'Ženski'),
+)
+DISTRICT_CHOICES = (
+	('1', ''),
+    ('2', '05600 - VIČ-RUDNIK'),
+    ('3', '05470 - ŠIŠKA'),
+	('4', '05030 - BEŽIGRAD'),
+    ('5', '05300 - MOSTE-POLJE'),
+)
 class RegistrationFrom(forms.Form):
    cardNumber = forms.CharField(label='Številka kartice:', max_length=100, widget=forms.NumberInput(attrs={'class': 'form-control'}))
    surname = forms.CharField(label='Priimek:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -16,5 +26,12 @@ class RegistrationFrom(forms.Form):
    phone = forms.CharField(label='Telefon:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
    password = forms.CharField(label='Geslo',max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
    password2 = forms.CharField(label='Ponovi geslo',max_length=100, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-   birthDate = forms.DateField()
-   
+   sex = forms.ChoiceField(choices=SEX_CHOICES)
+   district = forms.ChoiceField(choices=DISTRICT_CHOICES)
+   contact_surname = forms.CharField(label='Priimek:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+   contact_name = forms.CharField(label='Ime:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+   contact_address = forms.CharField(label='Naslov:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+   contact_phone_number = forms.CharField(label='Telefon', max_length=100, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+   contact_sorodstvo = forms.CharField(label='Naslov:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+   birthDate = forms.DateField(widget=forms.TextInput(attrs={'class':'datepicker'}))
+   postCode =  forms.CharField(label='Pošta', max_length=100, widget=forms.NumberInput(attrs={'class': 'form-control'}))

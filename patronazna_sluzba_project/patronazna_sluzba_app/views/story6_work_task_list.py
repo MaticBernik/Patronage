c78b694f9a868a1e86c52a2d3da5ed900d3b20ca
roberttovornik <robert.tovornik@gmail.com>
2017-05-05 05:34:35 +0200

    Pregled delovnih nalogov

--- patronazna_sluzba_project/patronazna_sluzba_app/views/story6_work_task_list.py
+++ patronazna_sluzba_project/patronazna_sluzba_app/views/story6_work_task_list.py
@@ -12,11 +12,36 @@ from django.urls import reverse
 from ipware.ip import get_ip #pip install django-ipware
 from patronazna_sluzba_app import token
 from patronazna_sluzba_app.forms import AddNursingPatientForm, ChangePasswordForm, LoginForm, PatientRegistrationFrom, RegisterMedicalStaffForm, WorkTaskForm
-from patronazna_sluzba_app.models import Izvajalec_ZS, Pacient, Patronazna_sestra, Sodelavec_ZD, User, Vodja_PS, Zdravnik
+from patronazna_sluzba_app.models import Izvajalec_ZS, Pacient, Patronazna_sestra, Sodelavec_ZD, User, Vodja_PS, Zdravnik, Obisk, Delovni_nalog
 import csv
 import django.contrib.auth
 import logging
 import os
 
-def list_work_task():
-    return render(request, 'work_task_list.html')
+def is_doctor(user):
+    if Zdravnik.objects.filter(uporabniski_profil=user).exists():
+        return True
+    return False
+
+def is_leader_ps(user):
+    if Vodja_PS.objects.filter(uporabniski_profil=user).exists():
+        return True
+    return False
+
+def list_work_task(request):
+    uporabnik = request.user
+    # obiski = Obisk.objects
+    # if is_doctor(uporabnik):
+        # delovni_nalogi = Delovni_nalog.objects.filter(zdravnik=uporabnik)
+    # elif is_leader_ps(uporabnik):
+        # delovni_nalogi = Delovni_nalog.objects.filter(vodja_PS=uporabnik)
+    # else:
+        # print("ERROR!!")
+        # return
+
+    obiski = Obisk.objects.all()
+    delovni_nalogi = Delovni_nalog.objects.all() 
+    context = {'work_task_list':delovni_nalogi, 'visits_list':obiski}
+    return render(request, 'work_task_list.html', context)
+    
+    

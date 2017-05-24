152374f462e7e3df0a5d5d73c3ff88f1bf450183
EneJsi <stoklas.nac@gmail.com>
2017-05-24 10:32:18 +0200

    Update patient BACK function

--- patronazna_sluzba_project/patronazna_sluzba_app/views/story15_update_patient_profile.py
+++ patronazna_sluzba_project/patronazna_sluzba_app/views/story15_update_patient_profile.py
@@ -11,6 +11,35 @@ from math import floor
 
 
 #   Funkcija, ki posodobi profil pacienta
-def update_patient(user, st_kartice, telefonska, naslov, spol, datum_rojstva, ime, priimek, email, okolis_id, skrbnistvo_id):
+def update_patient(user, st_kartice, telefonska, naslov, spol, datum_rojstva, ime, priimek, email, okolis, skrbnistvo, kontakt, posta, sorodstvo):
+    patient_profile = Pacient.objects.get(uporabniski_profil=user)
+    if st_kartice == '':
+        st_kartice = patient_profile.st_kartice
+    if telefonska == '':
+        telefonska = patient_profile.telefonska
+    if naslov == '':
+        naslov = patient_profile.naslov
+    if spol == '':
+        spol = patient_profile.spol
+    if datum_rojstva == '':
+        datum_rojstva = patient_profile.datum_rojstva
+    if ime == '':
+        ime = patient_profile.ime
+    if priimek == '':
+        priimek = patient_profile.priimek
+    if email == '':
+        email = patient_profile.email
+    if okolis == '':
+        okolis = patient_profile.okolis
+    if skrbnistvo == '':
+        skrbnistvo = patient_profile.skrbnistvo
+    if kontakt == '':
+        kontakt = patient_profile.kontakt
+    if posta == '':
+        posta = patient_profile.posta
+    if sorodstvo == '':
+        sorodstvo = patient_profile.sorodstvo
 
-    return 1
+    prof = patient_profile(st_kartice=st_kartice, telefonska_st=telefonska, naslov=naslov, spol=spol, datum_rojstva=datum_rojstva, ime=ime,
+                           priimek=priimek, email=email, kontakt=kontakt, posta=posta, okolis=okolis, skrbnistvo=skrbnistvo, sorodstvo=sorodstvo)
+    return prof

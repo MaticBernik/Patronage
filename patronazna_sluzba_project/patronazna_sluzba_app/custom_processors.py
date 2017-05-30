e7950177708381d5c0a7c9740934a38d2ea54120
roberttovornik <robert.tovornik@gmail.com>
2017-05-30 22:44:02 +0200

    Navbar update + Admin login fix

--- patronazna_sluzba_project/patronazna_sluzba_app/custom_processors.py
+++ patronazna_sluzba_project/patronazna_sluzba_app/custom_processors.py
@@ -23,9 +23,14 @@ def navbar_list_processor(request):
         add_medical_staff_arr = ["link_register_medical_staff", "Dodaj zdravstveno osebje", "add_medic", "glyphicon-plus" ]
         add_nursing_patient_arr = ["link_add_nursing", "Dodajte oskrbovano osebo", "add_nursing", "glyphicon-plus" ]
         change_password_arr = ["link_change_password", "Sprememba gesla", "chng_pass", "glyphicon-erase" ]
-        create_work_task_arr = ["link_work_task", "Ustvarite delovni nalog", "c_wrk_tsk", "glyphicon-file" ]
         
+        create_work_task_arr = ["link_work_task", "Ustvarite delovni nalog", "c_wrk_tsk", "glyphicon-file" ]
+        create_substitution_arr = ["substitution", "Ustvarite nadomeščanje", "c_subs", "glyphicon-retweet" ]
+        end_substitution_arr = ["link_sub_finished", "Zaključek nadomeščanja", "c_end_subs", "glyphicon-ok-circle" ]
+
+
         visitations_nurse_data_arr = ["link_visitations_nurse_data", "Vnos podatkov o obisku", "v_nrs_visits_data", "glyphicon-pencil" ] 
+
 
         view_substitutes_arr = ["link_empty", "Nadomeščanje", "v_subs", "glyphicon-th-list" ]
         view_visitations_arr = ["link_list_visitations", "Pregled obiskov", "v_visits", "glyphicon-th-list" ] 
@@ -34,18 +39,44 @@ def navbar_list_processor(request):
         visitation_planning_arr = ["link_plan_visit", "Planiranje obiskov", "v_plan_visits", "  glyphicon glyphicon-calendar" ]
         
         # adapt the list based on user role and task privleges
+        dropdowns_list = []
+
+        dropdown_worktasks = []
+        dropdown_visitations = []
+        dropdown_substitutions = []
+        dropdown_settings = []
+        dropdown_staff_management = []
+        
+
         if is_admin(user):
             role="Admin"
-            link_list = [control_panel_arr, arr_add_medical_staff, change_password_arr]
+            link_list = [control_panel_arr]
+            dropdown_settings.append(change_password_arr)
+            dropdown_staff_management.append(add_medical_staff_arr)
         elif is_doctor(user):
             role="Zdravnik"
-            link_list = [control_panel_arr, create_work_task_arr, view_work_tasks_arr, view_visitations_arr, change_password_arr]
+            link_list = [control_panel_arr]
+            dropdown_worktasks.append(create_work_task_arr)
+            dropdown_worktasks.append(view_work_tasks_arr)
+            dropdown_visitations.append(view_visitations_arr)
+            dropdown_settings.append(change_password_arr)
         elif is_leader_ps(user):
             role="Vodja PS"
-            link_list = [control_panel_arr, create_work_task_arr, view_work_tasks_arr, view_visitations_arr, change_password_arr]
+            link_list = [control_panel_arr]
+            dropdown_worktasks.append(create_work_task_arr)
+            dropdown_worktasks.append(view_work_tasks_arr)
+            dropdown_visitations.append(view_visitations_arr)
+            dropdown_substitutions.append(create_substitution_arr)
+            dropdown_substitutions.append(end_substitution_arr)
+            dropdown_settings.append(change_password_arr)
         elif is_nurse(user):
             role="medicinska sestra"
-            link_list = [control_panel_arr, view_work_tasks_arr, visitation_planning_arr, view_visitations_arr, visitations_nurse_data_arr, change_password_arr]
+            link_list = [control_panel_arr]
+            dropdown_worktasks.append(view_work_tasks_arr)
+            dropdown_visitations.append(view_visitations_arr)
+            dropdown_visitations.append(visitation_planning_arr)
+            dropdown_visitations.append(visitations_nurse_data_arr)
+            dropdown_settings.append(change_password_arr)
         elif is_coworker(user):
             role="Sodelavec"
             link_list = [control_panel_arr, change_password_arr ]
@@ -54,6 +85,22 @@ def navbar_list_processor(request):
             link_list = [control_panel_arr, add_nursing_patient_arr, change_password_arr ]
         
         oskrbovanci = None
+
+        if(len(dropdown_worktasks) > 0 ):
+            dropdowns_list.append([dropdown_worktasks, "Delovni nalogi"])
+
+        if(len(dropdown_visitations) > 0 ):
+            dropdowns_list.append([dropdown_visitations, "Obiski"])
+
+        if(len(dropdown_substitutions) > 0 ):
+            dropdowns_list.append([dropdown_substitutions, "Nadomeščanja"])
+
+        if(len(dropdown_staff_management) > 0 ):
+            dropdowns_list.append([dropdown_staff_management, "Osebje"])
+
+        if(len(dropdown_settings) > 0 ):
+            dropdowns_list.append([dropdown_settings, "Nastavitve"]) 
+
 
         if Pacient.objects.filter(uporabniski_profil=user).exists():
             print("User exits in patient filters")
@@ -65,7 +112,7 @@ def navbar_list_processor(request):
         else:
             print("user missing in patient filters")
         
-        return {'link_list': link_list, 'role': role, 'oskrbovanci_pacienta': oskrbovanci}
+        return {'dropdowns_list': dropdowns_list,'link_list': link_list, 'role': role, 'oskrbovanci_pacienta': oskrbovanci}
 
     else:
         return {}

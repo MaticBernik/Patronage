7084d710dc56f556825efae6c959aff71fbafa10
maticbernik <mb2551@student.uni-lj.si>
2017-05-31 20:17:20 +0200

    DB - sprememba zahteve za unique kombinacijo pri nadomescanjih

--- patronazna_sluzba_project/patronazna_sluzba_app/models.py
+++ patronazna_sluzba_project/patronazna_sluzba_app/models.py
@@ -434,7 +434,7 @@ class Zdravilo_DN(models.Model):
 
 class Nadomescanje(models.Model):
     class Meta:
-        unique_together = (('sestra', 'datum_zacetek','datum_konec'),)
+        unique_together = (('sestra', 'datum_zacetek','datum_konec', 'nadomestna_sestra'),)
 
     #vodja = models.ForeignKey(Vodja_PS,null=False) #vodja PS, ki je dodal nadomescanje
     sestra = models.ForeignKey(Patronazna_sestra,related_name='%(class)s_requests_created', null=False)

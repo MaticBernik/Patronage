7fb1196bf58bab969c4cc3aa0ea069dfa58368d3
maticbernik <mb2551@student.uni-lj.si>
2017-06-04 13:47:30 +0200

    V skripti popravljano uvazanje meritev in kreiranje porocil

--- data_import/import_data_to_DB.py
+++ data_import/import_data_to_DB.py
@@ -264,8 +264,8 @@ with open("TPO_Aktivnosti_patronazne_sestre.csv", "r") as aktivnosti_file:  # en
 	for line in aktivnosti_reader:
 		if len(line)==5:
 			print(line)
-			sifra = int(line[2])
-			sifra_storitve=int(line[0])
+			sifra = int(line[2]) #sifra aktivnosti
+			sifra_storitve=int(line[0]) #sifra storitve oz. vrste obiska
 
 			if sifra_storitve==30: #zdruzi sicer locena obiska otrocnice in novorojencka
 				sifra_storitve=20
@@ -289,6 +289,7 @@ with open("TPO_Aktivnosti_patronazne_sestre.csv", "r") as aktivnosti_file:  # en
 			meritev_id = cursor.fetchall()[0][0]
 
 			imena_polj=line[4].split(',')
+
 			for ime in imena_polj:
 				ime=ime.strip()
 				if len(ime)==0:
@@ -315,9 +316,12 @@ with open("TPO_Aktivnosti_patronazne_sestre.csv", "r") as aktivnosti_file:  # en
 				
 				enkraten_vnos=False;
 				if ime=='Prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
+
 				elif ime=='Datum':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				#elif ime == 'Moteno/Ni moteno':
 				#elif ime == 'Nizka/Srednja/Visoka':
 				elif ime == 'Sistolični (mm Hg)':
@@ -334,7 +338,8 @@ with open("TPO_Aktivnosti_patronazne_sestre.csv", "r") as aktivnosti_file:  # en
 					mozne_vrednosti='0,300'
 				elif ime == 'Datum rojstva otroka':
 					enkraten_vnos=True
-					continue
+					mozne_vrednosti='*'
+					#continue
 					'''if len(oskrbovanci)==0:
 						print("Napaka!")
 						continue
@@ -357,24 +362,33 @@ with open("TPO_Aktivnosti_patronazne_sestre.csv", "r") as aktivnosti_file:  # en
 				#elif ime == 'Da/Ne':
 				#elif ime == 'Ni posebnosti/Mikcija/Defekacija/Napenjanje/Kolike/Polivanje/Bruhanje':
 				elif ime == 'Urin: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'Blato: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'Vid: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'Vonj: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'Sluh: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'Okus: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'Otip: prosti vnos':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				#elif ime == 'Samostojen/Delno odvisen/Povsem odvisen':
 				elif ime == 'Pomoč: svojci':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime == 'drugi':
-					continue
+					mozne_vrednosti='*'
+					#continue
 				elif ime =='mmol/L':
 					mozne_vrednosti='0,500'
 				elif ime == '%':
@@ -567,6 +581,7 @@ cursor = conn.execute("select id,datum,opravljen,delovni_nalog_id,p_sestra_id fr
 obiski = cursor.fetchall()
 
 for obisk in obiski:
+	print("----- OBISK.delovni_nalog_id = "+str(obisk[3])+" ------ ")
 	id=int(obisk[0])
 	datum=datetime.strptime(obisk[1],"%Y-%m-%d %H:%M:%S")
 	cursor = conn.execute("select pacient_id from patronazna_sluzba_app_pacient_dn where delovni_nalog_id="+str(obisk[3])+";");
@@ -574,12 +589,16 @@ for obisk in obiski:
 	skrbniki=[]
 	oskrbovanci=[]
 	for pacient in pacienti:
-		cursor = conn.execute("select st_kartice from patronazna_sluzba_app_pacient where st_kartice=" + str(pacient) + " and uporabniski_profil_id=NULL;");
+		print("PACIENT: ",pacient)
+		cursor = conn.execute("select st_kartice from patronazna_sluzba_app_pacient where st_kartice='" + str(pacient) + "' and uporabniski_profil_id is Null;");
 		result = cursor.fetchall()
 		if len(result)==0:
 			skrbniki.append(pacient)
 		else:
 			oskrbovanci.append(pacient)
+	print("SKRBNIKI: ",skrbniki)
+	print("OSKRBOVANCI: ",oskrbovanci)
+
 	if datetime.now() > datum:
 		opravljen=True
 	else:
@@ -592,6 +611,7 @@ for obisk in obiski:
 	if opravljen:
 		cursor = conn.execute("select vrsta_obiska_id from patronazna_sluzba_app_delovni_nalog where id="+str(obisk[3])+";");
 		delovni_nalog_vrsta_obiska = cursor.fetchall()[0][0]
+		print("VRSTA OBISKA: ",delovni_nalog_vrsta_obiska)
 		cursor = conn.execute("select id from patronazna_sluzba_app_meritev where vrsta_obiska_id="+str(delovni_nalog_vrsta_obiska)+";");
 		meritve=cursor.fetchall()
 		meritve=[x[0] for x in meritve]
@@ -602,7 +622,7 @@ for obisk in obiski:
 			if len (polja_tmp)>0:
 				for x in polja_tmp:
 					polja.append(x[0])
-
+		print("STEVILO MERITEV: ",len(polja))
 		'''print("***SEZNAM PRIPADAJOCIH POLJ: ")
 		for polje in polja:
 			print(polje)'''
@@ -610,101 +630,120 @@ for obisk in obiski:
 			cursor = conn.execute("select id,ime,vnosno_polje,obvezno,mozne_vrednosti from patronazna_sluzba_app_polje_v_porocilu where id=" + str(polje_id) + ";");
 			polje_info = cursor.fetchall()[0]
 
-			if polje_info[3]==1: #Ustvari samo obvezna polja
-				if polje_info[1]=='Prosti vnos':
+		#if polje_info[3]==1: #Ustvari samo obvezna polja
+			if polje_info[1]=='Prosti vnos':
+				izbira = "Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1]=='Datum':
+				datum_tmp=datum-timedelta(days=random.choice([1,2,3,4,5,6,7]))
+			elif '/' in polje_info[1] and polje_info[1][polje_info[1].index('/')+1]!='L':
+				izbira=random.choice(str(polje_info[3]).split(','))
+			#elif polje_info[1] == 'Moteno/Ni moteno':
+			#elif polje_info[1] == 'Nizka/Srednja/Visoka':
+			elif polje_info[1] == 'Sistolični (mm Hg)':
+				izbira=90+int(random.random()*200-90)
+			elif polje_info[1] == 'Diastolični (mm Hg)':
+				izbira = 40 + int(random.random() * 90 - 40)
+			elif polje_info[1] == 'Udarci na minuto':
+				izbira=45+int(random.random()*140 - 45)
+			elif polje_info[1] == 'Vdihi na minuto':
+				izbira = 90 + int(random.random() * 80 - 90)
+			elif polje_info[1] == 'st C':
+				izbira = 36 + int(random.random() * 39 - 36)
+			elif polje_info[1] == 'kg':
+				izbira = 45 + (random.random() * 120 - 45)
+			elif polje_info[1] == 'Datum rojstva otroka':
+				if len(oskrbovanci)==0:
+					print("Napaka!")
 					continue
-				elif polje_info[1]=='Datum':
-					datum_tmp=datum-timedelta(days=random.choice([1,2,3,4,5,6,7]))
-				elif '/' in polje_info[1] and polje_info[1][polje_info[1].index('/')+1]!='L':
-					izbira=random.choice(str(polje_info[3]).split(','))
-				#elif polje_info[1] == 'Moteno/Ni moteno':
-				#elif polje_info[1] == 'Nizka/Srednja/Visoka':
-				elif polje_info[1] == 'Sistolični (mm Hg)':
-					izbira=90+int(random.random()*200-90)
-				elif polje_info[1] == 'Diastolični (mm Hg)':
-					izbira = 40 + int(random.random() * 90 - 40)
-				elif polje_info[1] == 'Udarci na minuto':
-					izbira=45+int(random.random()*140 - 45)
-				elif polje_info[1] == 'Vdihi na minuto':
-					izbira = 90 + int(random.random() * 80 - 90)
-				elif polje_info[1] == 'st C':
-					izbira = 36 + int(random.random() * 39 - 36)
-				elif polje_info[1] == 'kg':
-					izbira = 45 + (random.random() * 120 - 45)
-				elif polje_info[1] == 'Datum rojstva otroka':
-					if len(oskrbovanci)==0:
-						print("Napaka!")
-						continue
+				pacient_id=oskrbovanci[0]
+				print("ST KARTICEE: ",pacient_id)
+
+				'''cursor = conn.execute("select st_kartice from patronazna_sluzba_app_pacient;")
+				test=cursor.fetchall()
+				for t in test:
+					print("**Pacient v bazi: ",t[0])'''
+
+				cursor = conn.execute("select datum_rojstva from patronazna_sluzba_app_pacient where st_kartice='" + str(
+				pacient_id) +"';");
+				datum_rojstva=datetime.strptime(cursor.fetchall()[0][0],"%Y-%m-%d %H:%M:%S")
+				izbira=datum_rojstva
+
+			elif polje_info[1] == 'Porodna teža otroka (g)':
+				if len(oskrbovanci)==0:
+					print("Napaka!!!")
+					continue
+				pacient_id=oskrbovanci[0]
+				izbira = 2000 + int(random.random() * 6400 - 2000)
+			elif polje_info[1] == 'Porodna višina otroka (cm)':
+				if len(oskrbovanci)==0:
+					print("Napaka!!!")
+					continue
+				pacient_id=oskrbovanci[0]
+				izbira = 40 + int(random.random() * 65 - 40)
+			elif polje_info[1] == 'g':
+				if len(oskrbovanci)==0:
+					print("Napaka???")
+					#continue
+				pacient_id=oskrbovanci[0]
+				izbira = 2600 + int(random.random() * 8200 - 2600)
+			elif polje_info[1] == 'cm':
+				if len(oskrbovanci)==0:
+					print("Napaka???")
+					#continue
+				pacient_id=oskrbovanci[0]
+				izbira = 48 + int(random.random() * 80 - 48)
+			#elif polje_info[1] == 'Da/Ne':
+			#elif polje_info[1] == 'Ni posebnosti/Mikcija/Defekacija/Napenjanje/Kolike/Polivanje/Bruhanje':
+			elif polje_info[1] == 'Urin: prosti vnos':
+				izbira = "Urin - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'Blato: prosti vnos':
+				izbira = "Blato - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'Vid: prosti vnos':
+				izbira = "Vid - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'Vonj: prosti vnos':
+				izbira = "Vonj - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'Sluh: prosti vnos':
+				izbira = "Sluh - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'Okus: prosti vnos':
+				izbira = "Okus - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'Otip: prosti vnos':
+				izbira = "Otip - Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			#elif polje_info[1] == 'Samostojen/Delno odvisen/Povsem odvisen':
+			elif polje_info[1] == 'Pomoč: svojci':
+				izbira = "Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] == 'drugi':
+				izbira = "Porocilo o pacientovem stanju. Testni vnos."
+				#continue
+			elif polje_info[1] =='mmol/L':
+				izbira = 50 + int(random.random() * 230 - 50)
+			elif polje_info[1] == '%':
+				izbira = 85 + int(random.random() * 100 - 85)
+			#elif polje_info[1] == 'Da/Delno/Ne':
+			else:
+				izbira = "Porocilo o pacientovem stanju. Testni vnos."
+
+
+
+			for m in meritve:
+				cursor = conn.execute("select id,sifra,opis,vrsta_obiska_id from patronazna_sluzba_app_meritev where id=" + str(m) + ";");
+				meritev_info = cursor.fetchall()[0]
+				if meritev_info[3]==30:
 					pacient_id=oskrbovanci[0]
-					cursor = conn.execute("select datum_rojstva from patronazna_sluzba_app_pacient where st_kartice=" + str(
-					pacient_id) +";");
-					datum_rojstva=datetime.strptime(cursor.fetchall()[0][0],"%Y-%m-%d %H:%M:%S")
-					izbira=datum_rojstva
-
-				elif polje_info[1] == 'Porodna teža otroka (g)':
-					if len(oskrbovanci)==0:
-						print("Napaka!")
-						continue
-					pacient_id=oskrbovanci[0]
-					izbira = 2000 + int(random.random() * 6400 - 2000)
-				elif polje_info[1] == 'Porodna višina otroka (cm)':
-					if len(oskrbovanci)==0:
-						print("Napaka!")
-						continue
-					pacient_id=oskrbovanci[0]
-					izbira = 40 + int(random.random() * 65 - 40)
-				elif polje_info[1] == 'g':
-					if len(oskrbovanci)==0:
-						print("Napaka!")
-						continue
-					pacient_id=oskrbovanci[0]
-					izbira = 2600 + int(random.random() * 8200 - 2600)
-				elif polje_info[1] == 'cm':
-					if len(oskrbovanci)==0:
-						print("Napaka!")
-						continue
-					pacient_id=oskrbovanci[0]
-					izbira = 48 + int(random.random() * 80 - 48)
-				#elif polje_info[1] == 'Da/Ne':
-				#elif polje_info[1] == 'Ni posebnosti/Mikcija/Defekacija/Napenjanje/Kolike/Polivanje/Bruhanje':
-				elif polje_info[1] == 'Urin: prosti vnos':
-					continue
-				elif polje_info[1] == 'Blato: prosti vnos':
-					continue
-				elif polje_info[1] == 'Vid: prosti vnos':
-					continue
-				elif polje_info[1] == 'Vonj: prosti vnos':
-					continue
-				elif polje_info[1] == 'Sluh: prosti vnos':
-					continue
-				elif polje_info[1] == 'Okus: prosti vnos':
-					continue
-				elif polje_info[1] == 'Otip: prosti vnos':
-					continue
-				#elif polje_info[1] == 'Samostojen/Delno odvisen/Povsem odvisen':
-				elif polje_info[1] == 'Pomoč: svojci':
-					continue
-				elif polje_info[1] == 'drugi':
-					continue
-				elif polje_info[1] =='mmol/L':
-					izbira = 50 + int(random.random() * 230 - 50)
-				elif polje_info[1] == '%':
-					izbira = 85 + int(random.random() * 100 - 85)
-				#elif polje_info[1] == 'Da/Delno/Ne':
+					break
+				if meritev_info[3]==80:
+					pacient_id=skrbniki[0]
 
 
-
-				for m in meritve:
-					cursor = conn.execute("select id,sifra,opis,vrsta_obiska_id from patronazna_sluzba_app_meritev where id=" + str(m) + ";");
-					meritev_info = cursor.fetchall()[0]
-					if meritev_info[3]==30:
-						pacient_id=oskrbovanci[0]
-						break
-					if meritev_info[3]==80:
-						pacient_id=skrbniki[0]
-
-
-				conn.execute("INSERT INTO patronazna_sluzba_app_porocilo_o_obisku (obisk_id, pacient_id, polje_id, vrednost) VALUES (?,?,?,?)", (int(id), pacient_id, int(polje_info[0]),str(izbira)));
+			conn.execute("INSERT INTO patronazna_sluzba_app_porocilo_o_obisku (obisk_id, pacient_id, polje_id, vrednost) VALUES (?,?,?,?)", (int(id), pacient_id, int(polje_info[0]),str(izbira)));
 
 
 
